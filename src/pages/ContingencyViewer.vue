084051472749e5bd8dfa641748ce59ee5d188bde
NataliaUrrutiaAgudo <150816023+natis25@users.noreply.github.com>
2025-11-10 16:43:53 -0400

    Cambio de link

--- src/pages/ContingencyViewer.vue
+++ src/pages/ContingencyViewer.vue
@@ -35,7 +35,7 @@ function goBack() {
     <div class="bpwin-container">
       <div class="aspect-ratio-box">
         <iframe
-          src="https://drive.google.com/file/d/10vXBxoBUDShMpjf_XsOvjK2Y-57no61v/view?usp=sharing"
+          src="https://drive.google.com/file/d/10vXBxoBUDShMpjf_XsOvjK2Y-57no61v/view?usp=drive_link"
           class="bpwin-iframe"
           title="Google Sheets Report"
         />
